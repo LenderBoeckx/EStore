ac4b040fad5a1a12d62fe14452519cd1ae3724f1
r06658604 <r0658604@student.thomasmore.be>
2025-01-15 22:54:27 +0100

    models aangemaakt voor product en pagination

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    naam: string;
+    beschrijving: string;
+    prijs: number;
+    fotoURL: string;
+    type: string;
+    merk: string;
+    hoeveelheidInVoorraad: number;
+}
