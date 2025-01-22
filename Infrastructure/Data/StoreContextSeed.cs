6f2fce437a5ec4a7eb9dc639017800432f8ee5af
r06658604 <r0658604@student.thomasmore.be>
2025-01-22 16:21:37 +0100

    Pre continuous integration

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using Core.Entities;
@@ -10,10 +11,12 @@ public class StoreContextSeed
     //dummy data toevoegen aan de database bij opstarten van de applicatie
     public static async Task SeedAsync(StoreContext context)
     {
+        var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        
         //als er geen data aanwezig is in de producten entiteit dan wordt deze toegevoegd vanuit het json bestand
         if(!context.Products.Any())
         {
-            var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+            var productsData = await File.ReadAllTextAsync(path + @"/Data/SeedData/products.json");
 
             var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
@@ -28,7 +31,7 @@ public class StoreContextSeed
         //als er geen data aanwezig is in de deliverymethods entiteit dan wordt deze toegevoegd vanuit het json bestand
         if(!context.DeliveryMethods.Any())
         {
-            var deliveryData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/delivery.json");
+            var deliveryData = await File.ReadAllTextAsync(path + @"/Data/SeedData/delivery.json");
 
             var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryData);
 
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production : true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51QifAMPvLLzALKW1wKmIA9CsKJH6wrnKdvACvGjGRZWCdbWU7U2sIYxvrKH9lE6TAbzwrpbLqD81KpvrlyM8miE300DLsxydbg'
+    stripePublicKey: 'pk_test_51Qk4moIRl3Lbkdmx4Vkp8Q0sDqXd1UeD5xFIkqhTtPFiwC97d4CIindzMuvNbR0xONwcZPPheh9WUfqf0NnDrhok00w1l55X2S'
 };
