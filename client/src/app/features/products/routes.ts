277eeda8c98ac24611dc2a5114313cc2600d9a08
r06658604 <r0658604@student.thomasmore.be>
2025-01-28 17:02:07 +0100

    guards toegepast op de product routes

--- client/src/app/features/products/routes.ts
+++ client/src/app/features/products/routes.ts
@@ -1,6 +1,8 @@
 import { Route } from "@angular/router";
 import { CreateProductComponent } from "./create-product/create-product.component";
+import { adminGuard } from "../../core/guards/admin.guard";
+import { authGuard } from "../../core/guards/auth.guard";
 
 export const productRoutes: Route[] = [
-    {path: 'create-product', component: CreateProductComponent}
+    {path: 'create-product', component: CreateProductComponent, canActivate: [authGuard, adminGuard]}
 ]
